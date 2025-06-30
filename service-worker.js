f80021cd5361df06350dfc6f805da04d22bef3d1
Victor Henrique <vhramalho16@gmail.com>
2025-06-30 15:49:38 -0300

    Teste de app PWA com GIT HUB PAGES

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "pdv-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/index_script.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
